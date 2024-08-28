cc4f5932cc6593307d3d4dacd30d15cd0039a315
Saidmurod Sabirov <sabir.dev7@gmail.com>
2024-08-28 13:43:41 +0300

    contact section social media links added

--- components/Landing/Contacts.vue
+++ components/Landing/Contacts.vue
@@ -12,7 +12,7 @@
       </p>
       <div class="container mx-auto">
         <div
-          class="grid grid-cols-1 gap-12 px-4 mt-10 md:grid-cols-2 lg:grid-cols-3"
+          class="grid grid-cols-1 gap-12 px-4 mt-10 md:grid-cols-2 lg:grid-cols-4"
         >
           <div class="flex flex-col items-start justify-center text-left">
             <span class="p-3 text-primary rounded-full bg-light">
@@ -87,7 +87,82 @@
 
             <h2 class="mt-4 text-lg font-medium text-light">Phone</h2>
             <p class="mt-2 text-light/90">Mon-Fri from 8am to 5pm.</p>
-            <p class="mt-2 text-light/80">+1 (555) 000-0000</p>
+            <p class="mt-2 text-light/80">+212-456-7890</p>
+          </div>
+
+          <div class="flex flex-col items-start justify-center text-left">
+            <span class="p-3 text-primary rounded-full bg-light">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+                />
+              </svg>
+            </span>
+
+            <h2 class="mt-4 text-lg font-medium text-light">Social Media</h2>
+            
+            <p class="mt-2 text-light/90">Follow us on</p>
+            <div class="mt-2 text-light/90 flex space-x-2 ">
+              <a href="#" class="text-2xl hover:text-tertiary transition-all duration-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22 22 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202z"
+                  />
+                </svg>
+              </a>
+              <a href="#" class="text-2xl hover:text-tertiary transition-all duration-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                >
+                  <circle cx="4.983" cy="5.009" r="2.188" fill="currentColor" />
+                  <path
+                    fill="currentColor"
+                    d="M9.237 8.855v12.139h3.769v-6.003c0-1.584.298-3.118 2.262-3.118c1.937 0 1.961 1.811 1.961 3.218v5.904H21v-6.657c0-3.27-.704-5.783-4.526-5.783c-1.835 0-3.065 1.007-3.568 1.96h-.051v-1.66zm-6.142 0H6.87v12.139H3.095z"
+                  />
+                </svg>
+              </a>
+              <a href="#" class="text-2xl hover:text-tertiary transition-all duration-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248a4.623 4.623 0 0 0 0-9.248m0 7.627a3.004 3.004 0 1 1 0-6.008a3.004 3.004 0 0 1 0 6.008"
+                  />
+                  <circle
+                    cx="16.806"
+                    cy="7.207"
+                    r="1.078"
+                    fill="currentColor"
+                  />
+                  <path
+                    fill="currentColor"
+                    d="M20.533 6.111A4.6 4.6 0 0 0 17.9 3.479a6.6 6.6 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.6 6.6 0 0 0-2.184.42a4.6 4.6 0 0 0-2.633 2.632a6.6 6.6 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71s0 2.753.056 3.71c.015.748.156 1.486.419 2.187a4.6 4.6 0 0 0 2.634 2.632a6.6 6.6 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.6 6.6 0 0 0 2.186-.419a4.6 4.6 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186c.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.6 6.6 0 0 0-.421-2.217m-1.218 9.532a5 5 0 0 1-.311 1.688a3 3 0 0 1-1.712 1.711a5 5 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055c-2.438 0-2.687 0-3.655-.055a5 5 0 0 1-1.669-.311a3 3 0 0 1-1.719-1.711a5.1 5.1 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654s0-2.686.053-3.655a5 5 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5 5 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a5 5 0 0 1 1.67.311a3 3 0 0 1 1.712 1.712a5.1 5.1 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655s0 2.698-.043 3.654z"
+                  />
+                </svg>
+              </a>
+            </div>
           </div>
         </div>
       </div>
