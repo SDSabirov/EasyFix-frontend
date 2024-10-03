b280df2d6468ed81cddc91324efdfe169435b65e
Saidmurod Sabirov <sabir.dev7@gmail.com>
2024-10-03 19:43:57 +0300

    SEO title description keywords

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { resolve } from 'path';
+import { resolve } from "path";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  
+
   alias: {
-    '@': resolve(__dirname),
-    '~': resolve(__dirname),
+    "@": resolve(__dirname),
+    "~": resolve(__dirname),
   },
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-    }
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+    },
   },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   postcss: {
     plugins: {
@@ -25,23 +25,22 @@ export default defineNuxtConfig({
   },
   build: {
     rollupOptions: {
-      external: ['axios'], // Add axios here
+      external: ["axios"], // Add axios here
     },
   },
 
-  modules: ['@nuxtjs/seo'],
-  
+  modules: ["@nuxtjs/seo"],
+
   site: {
-    url: 'https://easyfixappliance.com',
-    name: 'Expert Appliance Repair in San Mateo, Palo Alto, and Nearby Areas | Specializing in Sub-Zero, Wolf, Viking and more',
-    description: 'Trusted Appliance Repair and Service in Atherton, Belmont, Burlingame, Emerald, San Francisco, and surrounding areas. From Sub-Zero to Viking, our skilled technicians provide fast, reliable repairs using original parts. Serving homes in Mountain View, Palo Alto, San Mateo, and more. Schedule your service today!',
-    defaultLocale: 'en', // not needed if you have @nuxtjs/i18n installed
+    url: "https://easyfixappliance.com",
+    name: "Easy Fix Appliance | Trusted Sub-Zero, Wolf, & Viking Appliance Repair in San Mateo, Palo Alto, and Surroundings in Bay area",
+    description:
+      "Sub-Zero, Wolf, & Viking Appliance Repair and Service near Atherton, Belmont, Burlingame, Emerald, San Francisco, and nearby areas. From Sub-zero Refrigerators and Freezers to Viking Cooking Appliances, our skilled technicians provide fast, reliable repairs using original parts. Serving homes in Mountain View, Palo Alto, San Mateo, and more. Schedule your service today!",
+    defaultLocale: "en", // not needed if you have @nuxtjs/i18n installed
   },
   robots: {
-    UserAgent: '*', // Applies to all user agents
-    Disallow: '/admin', // Disallow access to admin pages
-    Allow: '/' // Allow access to all other pages
+    UserAgent: "*", // Applies to all user agents
+    Disallow: "/admin", // Disallow access to admin pages
+    Allow: "/", // Allow access to all other pages
   },
-
-  
-})
+});
