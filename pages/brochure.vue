358367413b84a14ea3f7ec99e7ba56134caeaef5
Saidmurod Sabirov <sabir.dev7@gmail.com>
2025-01-24 04:50:09 +0000

    promotion brochure

---
+++ pages/brochure.vue
@@ -0,0 +1,266 @@
+<template>
+  <div>
+    <div class="fixed w-full top-0 z-50 bg-primary text-white shadow-xl">
+      <a href="tel:4159414144">
+        <div
+          class="container mx-auto px-4 py-3 flex justify-center items-center max-w-screen-xl"
+        >
+          <div
+            class="text-xl lg:text-base font-medium flex space-x-4 items-center"
+          >
+            <p>
+              Get Same Day or Next Day Service to Bay Area.
+              <span>Call Now (415) 941-4144</span>
+            </p>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <div class="w-full flexm flex-col items-center justify-center mt-10">
+      <div class="max-w-screen-xl w-full mx-auto">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-12 px-2 md:px-0 py-12">
+          <div class="flex flex-col space-y-8">
+            <h2 class="text-gray-600 text-lg capitalize font-semibold">
+              #1 appliance repair in Bay Area
+            </h2>
+            <p class="text-primary text-4xl uppercase font-semibold">
+              GET $25 OFF ALL APPLIANCE REPAIRS WHEN YOU CALL BEFORE 5PM.
+            </p>
+            <p class="text-gray-600 text-lg capitalize font-semibold">
+              No matter where you’re located in the Bay area, our neighborhood
+              technicians are only minutes away.
+            </p>
+            <a
+              href="tel:4159414144"
+              class="p-4 text-white bg-primary rounded-xl text-2xl text-center hover:text-xl transition-all duration-300 ease-in-out"
+              >Call Now (415) 941-4144</a
+            >
+            <div class="flex space-x-4 items-center">
+              <img
+                src="/assets/icons/googleReviews.png"
+                alt="Google reviews"
+                class="w-[100px]"
+              />
+              <p class="text-gray-600 text-lg capitalize font-semibold">
+                5 Star service based on our customers
+              </p>
+            </div>
+          </div>
+
+          <div class="rounded-xl w-full h-full overflow-hidden">
+            <img
+              src="/assets/img/kitchenBright.webp"
+              alt="kitchen image"
+              class="object-cover"
+            />
+          </div>
+        </div>
+        <div class="flex justify-center mb-6">
+          <div
+            class="w-full flex space-x-6 space-y-4 md:space-y-0 flex-wrap flex-grow items-center justify-center"
+          >
+            <div class="max-h-[40px]">
+              <img
+                src="/assets/img/kitchenaid.png"
+                class="h-[40px] object-contain max-w-[80px]"
+                alt="kitchenaid logo"
+              />
+            </div>
+
+            <div class="max-h-[40px]">
+              <img
+                src="/assets/img/subzeroLogosilver.png"
+                class="h-[40px] object-contain max-w-[100px]"
+                alt="Subzero Logo"
+              />
+            </div>
+            <div class="max-h-[40px]">
+              <img
+                src="/assets/img/wolf.png"
+                class="h-[40px] object-contain max-w-[100px]"
+                alt="Wolf Logo"
+              />
+            </div>
+            <div class="max-h-[40px]">
+              <img
+                src="/assets/img/GEmonogram.png"
+                class="h-[40px] object-contain max-w-[100px]"
+                alt="GE Monogram Logo"
+              />
+            </div>
+            <div class="max-h-[40px]">
+              <img
+                src="/assets/img/viking.png"
+                class="h-[40px] object-contain max-w-[100px]"
+                alt="viking logo"
+              />
+            </div>
+            <div class="max-h-[40px]">
+              <img
+                src="/assets/img/dacor-logo.png"
+                class="h-[40px] object-contain max-w-[100px]"
+                alt="Dacor Logo"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section
+      class="w-full flex flex-col space-y-6 items-center justify-center py-8 mt-8 px-4 md:px-0"
+    >
+      <div
+        class="grid grid-cols-1 md:grid-cols-2 max-w-screen-xl w-full px-4 gap-6"
+      >
+        <div class="flex flex-col w-full space-y-4">
+          <h2
+            class="text-4xl md:text-6xl text-primary w-full font-bold leading-loose text-justify"
+          >
+            Free Diagnostic with Repair Hurry, Call Now!
+          </h2>
+          <a
+            href="tel:4159414144"
+            class="text-4xl text-primary underline font-semibold mt-2"
+            >(415) 941-4144
+          </a>
+          <p class="text-xl text-gray-900">
+            Nothing’s worse than a broken appliance—except waiting forever for
+            the repair
+          </p>
+          <p class="text-xl text-gray-900">
+            At Easyfix, we understand how frustrating it is when your daily
+            routine grinds to a halt because of a faulty appliance. The last
+            thing you need is more delays waiting on a repair. That’s why we
+            pride ourselves on fast, reliable service—so you can get back to
+            your normal routine without the long wait.
+          </p>
+        </div>
+        <div class="rounded-xl border border-gray-100 shadow-2xl p-4 bg-light">
+          <div
+            class="border-2 border-primary flex flex-col space-y-4 rounded-xl w-full h-full p-4 py-8 uppercase"
+          >
+            <h2
+              class="text-4xl md:text-8xl font-bold w-full text-center text-primary"
+            >
+              $25 OFF
+            </h2>
+            <p class="text-4xl md:text-8xl w-full text-center text-primary">
+              Any Repair
+            </p>
+            <p class="text-2xl md:text-8xl w-full text-center text-primary">
+              When you call before 5pm
+            </p>
+            <div class="w-full flex px-6 justify-center">
+              <a href="" class="text-4xl bg-primary text-white rounded-xl p-4"
+                >Call Now (415) 941-4144</a
+              >
+            </div>
+            <p class="text-lg w-full text-center text-gray-600">
+              Offer good for completed repair only
+            </p>
+            <p class="text-lg w-full text-center text-gray-600">
+              Doesn't apply for diagnostic fee
+            </p>
+          </div>
+        </div>
+      </div>
+      <div
+        class="grid grid-cols-1 md:grid-cols-2 max-w-screen-xl w-full px-4 gap-6"
+      >
+        <div class="flex flex-col w-full space-y-4">
+          <h2
+            class="text-4xl md:text-6xl text-primary w-full font-bold leading-loose text-justify"
+          >
+            Why Choose Us?
+          </h2>
+
+          <ul class="text-xl text-primary list-disc">
+            <li>
+              <strong>You Get A LOCAL Appliance Repair Expert</strong> (No
+              matter where you are at in the Bay Area, our techs are only
+              minutes away)
+            </li>
+            <li>
+              <strong>You Get Friendly, Same Day Service Before 5pm</strong>
+              (Availibity is limited so CALL NOW)
+            </li>
+            <li>
+              <strong>Your Diagnostic Is FREE With Repair</strong> (If your
+              appliance gets fixed, you don't pay the diagnostic fee)
+            </li>
+            <li>
+              <strong>You Always Get 5-Star Service</strong> (if we don't
+              deserve 5 STARS, let us know and we'll do everything we can to
+              make it right)
+            </li>
+            <li>
+              <strong>You Can Relax</strong> (We're licensed, bonded, and
+              insured, plus we perform drug and background checks on all
+              employees)
+            </li>
+          </ul>
+        </div>
+        <div class="rounded-xl border border-gray-100 shadow-2xl p-4 bg-light">
+          <div
+            class="border-2 border-primary flex flex-col space-y-4 rounded-xl w-full h-full p-4 py-8 uppercase"
+          >
+            <h2
+              class="text-4xl md:text-8xl font-bold w-full text-center text-primary"
+            >
+              $25 OFF
+            </h2>
+            <p class="text-4xl md:text-8xl w-full text-center text-primary">
+              Any Repair
+            </p>
+            <p class="text-2xl md:text-8xl w-full text-center text-primary">
+              When you call before 5pm
+            </p>
+            <div class="w-full flex px-6 justify-center">
+              <a href="" class="text-4xl bg-primary text-white rounded-xl p-4"
+                >Call Now (415) 941-4144</a
+              >
+            </div>
+            <p class="text-lg w-full text-center text-gray-600">
+              Offer good for completed repair only
+            </p>
+            <p class="text-lg w-full text-center text-gray-600">
+              Doesn't apply for diagnostic fee
+            </p>
+          </div>
+        </div>
+      </div>
+      <div
+        class="max-w-screen-xl rounded-xl bg-primary w-full flex flex-col  items-ceneter justify-center md:flex-row space-x-6 p-4 mt-6 mb-6 space-y-6 md:space-y-0"
+      >
+        <div class="w-full md:w-1/5 flex items-center justify-center p-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="96"
+            height="96"
+            viewBox="0 0 24 24"
+            style="fill: rgba(255, 255, 255, 1); transform: ; msfilter: "
+          >
+            <path
+              d="m12 17 1-2V9.858c1.721-.447 3-2 3-3.858 0-2.206-1.794-4-4-4S8 3.794 8 6c0 1.858 1.279 3.411 3 3.858V15l1 2z"
+            ></path>
+            <path
+              d="m16.267 10.563-.533 1.928C18.325 13.207 20 14.584 20 16c0 1.892-3.285 4-8 4s-8-2.108-8-4c0-1.416 1.675-2.793 4.267-3.51l-.533-1.928C4.197 11.54 2 13.623 2 16c0 3.364 4.393 6 10 6s10-2.636 10-6c0-2.377-2.197-4.46-5.733-5.437z"
+            ></path>
+          </svg>
+        </div>
+        <div class="w-full md:w-3/5 flex items-center justify-center flex-col text-balance gap-4">
+            <h3 class="text-white text-4xl uppercase font-semibold text-center">Local Service Guarantee</h3>
+            <p class="text-white text-xl uppercase font-normal text-center">No matter where you are located, our neighborhood technicians are only minutes away</p>
+        </div>
+        <div class="w-full md:w-2/5 flex items-center justify-center flex-col text-balance">
+            <a href="tel:4159414144" class="text-primary text-xl p-4 rounded-xl bg-white">Call Now (415) 941-4144</a>
+        </div>
+      </div>
+    </section>
+    <Footer/>
+  </div>
+</template>
+
+<script setup></script>
