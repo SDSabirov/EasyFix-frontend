c588d8a7383425c9acbd6b38e765da59133a22e5
Saidmurod Sabirov <sabir.dev7@gmail.com>
2025-02-18 19:47:05 +0000

    gtm lazy load removed

--- app.vue
+++ app.vue
@@ -22,22 +22,18 @@ useHead(() => ({
 useHead({
   script: [
     {
-      // This JavaScript will be deferred until the user interacts
+      // This JavaScript loads GTM immediately when the script runs.
       children: `
-        document.addEventListener('DOMContentLoaded', function() {
-          const loadGTM = () => {
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5RH2D8CH');
-          };
-
-          // Lazy load GTM on user interaction (e.g., scroll or click)
-          ['scroll', 'click'].forEach(event => {
-            window.addEventListener(event, loadGTM, { once: true });
-          });
-        });
+        (function(w,d,s,l,i){
+          w[l]=w[l]||[];
+          w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+          var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),
+              dl=l!='dataLayer' ? '&l='+l : '';
+          j.async = true;
+          j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+          f.parentNode.insertBefore(j, f);
+        })(window,document,'script','dataLayer','GTM-5RH2D8CH');
       `,
       type: 'text/javascript'
     },
@@ -53,7 +49,6 @@ useHead({
     }
   ]
 });
-
 
 
 onMounted(() => {
